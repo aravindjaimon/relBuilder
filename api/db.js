ba74093023eb2b1408ad4147ddf6aa5cd77dadc8
Aravind Jaimon <aravindjaimon1@gmail.com>
2020-09-27 21:44:57 +0530

    updated to key

--- api/db.js
+++ api/db.js
@@ -1,12 +1,20 @@
 require("dotenv").config();
 const Pool = require("pg").Pool;
+const fs = require("fs");
 
 const pool = new Pool({
+  sslmode: "require",
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   host: process.env.PGHOST,
   port: process.env.PGPORT,
   database: process.env.PGDATABASE,
+  ssl: {
+    rejectUnauthorized: false,
+    cert: fs.readFileSync("./ca-certificate.crt").toString(),
+  },
 });
+
+const pool = new Pool({});
 
 module.exports = pool;
